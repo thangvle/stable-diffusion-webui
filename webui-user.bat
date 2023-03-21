2911b3bc0df2e760566df8cbb9b5b5e4003d2bfc
Thang Le <tug18152@temple.edu>
2023-03-22 01:37:27 +0700

    Adding conda env to run webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,10 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\tshot\anaconda3\envs\stable-diffusion-env\Scripts\python.exe
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR= C:\Users\tshot\anaconda3\envs\stable-diffusion-env
+set COMMANDLINE_ARGS=--precision full --xformers
+
+set OPTIMIZED_TURBO=true
 
 call webui.bat
